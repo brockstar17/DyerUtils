56bb9aa6dc2c1520f4d4caaa44ce967c8516d4e2
Brock <Brock@LAPTOP-G6N1Q8KF.mshome.net>
2017-05-10 10:53:41 -0400

    Fixed sorting method

--- DyerUtils/src/com/github/brockstar17/AlphabeticSort.java
+++ DyerUtils/src/com/github/brockstar17/AlphabeticSort.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 
 public class AlphabeticSort {
 
-	private static String[] alphaRef = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "g", "k", "l", "m", "n", "o", "p",
+	private static String[] alphaRef = { "a", "b", "c", "d",
+			"e", "f", "g", "h",
+			"i", "j", "k", "l", "m", "n", "o", "p",
 			"q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
 
 	/**
